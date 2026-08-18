c529c3b6c932e50f311bfd62b803e66d34bdb567
Qinjingxue <2940293874wang@gmail.com>
2026-08-19 01:05:25 +0800

    修复导入bug

--- sunpack.py
+++ sunpack.py
@@ -4,7 +4,7 @@ import sys
 # Ensure import path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from sunpack.entrypoint import main
+from sunpack.support.entrypoint import main
 
 if __name__ == "__main__":
     raise SystemExit(main())
